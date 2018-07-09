8ff20efd0a2e4c0b70760d0d0667085d46a96b4b
Rod <greasytaco@live.com>
2018-07-09 15:23:29 +0200

    Done

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # Overwrite-Reddit-Comments
 A simple script to quickly overwrite all your reddit comments
+
+It semi-automatize the overwriting of your reddit comments with random numbers. 
+
+Good in case you don't want random bits of public information floating out in the public internet forever.
+
+Reddit keeps all the original comments and edits backed up on their servers though.
---
+++ script.js
@@ -0,0 +1,70 @@
+/*
+STEP 0:
+
+Make sure you are logged in to your Reddit account and go to your comments history 
+and open your browser console (ctrl + shift + i on Chrome or Firefox)
+
+*/
+
+/*
+STEP 1:
+
+Copy and paste, in the browser console, the below code only up to MY next comment. 
+This will open all your comments for editing. Wait until all are open before proceeding to the next step.
+
+*/
+(function clickEdit() {
+    let currentTime = 0;
+    let aTags = document.querySelectorAll("a.CommentFlatList__item");
+    
+    for (const key of Object.keys(aTags)) {
+        if (aTags[key].textContent === "edit") {
+            currentTime = currentTime + 400;
+            setTimeout(function() {
+                aTags[key].click();
+            }, currentTime);
+            
+        }
+    } 
+})();
+
+/*
+STEP 2: 
+
+Copy and paste, in the browser console, the below code only up to MY next comment. 
+This will replace all your comments with a random number. Wait until all the
+comments are overwritten before proceeding to the next step.
+
+*/
+
+(function changeText() {
+    let textAreas = document.querySelectorAll("textarea.MarkdownForm__text");
+    let currentTime = 0;
+    for (const key of Object.keys(textAreas)) {
+        currentTime = currentTime + 400;
+        setTimeout(function() {
+            textAreas[key].value = Math.pow(Math.floor(Math.random() * 10) + 1, Math.floor(Math.random() * 10) + 1);
+        }, currentTime);
+    }
+})();
+
+/*
+STEP 3: 
+
+Copy and paste this last bit in the browser console
+This will save all your new comments. Wait until all the
+comments are saved before repeating this on the next comments page.
+Repeat steps 1 - 3 for all your comment pages.
+
+*/
+
+(function saveText() {
+    let saveButtons = document.querySelectorAll("button.MarkdownForm__submit");
+    let currentTime = 0;
+    for (const key of Object.keys(saveButtons)) {
+        currentTime = currentTime + 400;
+        setTimeout(function() {
+            saveButtons[key].click();
+        }, currentTime);
+    }
+})()
